cde711c1251dbf4a3dd39b181f32818880f9b163
Venere <50956992+veneresabrina@users.noreply.github.com>
2019-07-22 12:56:12 +0200

    Create highHR.m

---
+++ Server/Handle_Data/ThingspeakMatlab/highHR.m
@@ -0,0 +1,35 @@
+% Channel ID to read data from 
+readChannelID = 779308; 
+   
+% Channel Read API Key   
+readAPIKey = 'D86SV78XNDD5SKE0'; 
+
+[iHR,time] = thingSpeakRead(readChannelID,'Fields',2,'DateRange',[datetime('now')-30, datetime('now')],'ReadKey',readAPIKey); 
+
+flag = 0;
+j = 1;
+k = 1;
+%number of critical events:
+for i = 1:length(iHR)
+    if iHR(i) > 78 && flag == 0
+        crit(j) = datenum(time(i));
+        flag = 1;
+        j = j + 1;
+    end
+    if flag == 1 && iHR(i) < 68
+        ris(j - 1) = datenum(time(i));
+        flag = 0;
+    end
+    if i == length(iHR) && flag == 1
+        crit(length(crit)) = [];
+    end
+end
+
+for i1 = 1:length(crit)
+    tempo(i1) = etime(datevec(ris(i1)),datevec(crit(i1)));
+end
+
+bar(tempo); 
+xlabel('# of Critical events'); 
+ylabel('Number of seconds until resolution'); 
+title('Time of resolution of critical events \newline for high heart rate in the last 30 days');
